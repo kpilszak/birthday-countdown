a80b65ab141adff41cf0858a70fa24c66b748e96
kpilszak <klaudia.pilszak@gmail.com>
2023-03-30 06:09:12 -0400

    Prepare time values in milliseconds

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const timeLeft = document.getElementById('time-left');
 
 const birthday = new Date('07/15/2024');
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24; // milliseconds
 const today = new Date();
 
 console.log(birthday);
